1360424af6f4af11ea04ee3157de2b10988f2dcc
gian <gianfranco.s@gmail.com>
2025-05-18 09:59:02 -0300

    Move methods to db_mock

---
+++ src/db_mock.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+encrypted_store = pd.read_csv("/tmp/test_data.csv").to_dict(orient="records")
+
+
+def list_projects(data=encrypted_store):
+    return sorted({rec["project_id"] for rec in data})
+
+
+def fetch_project_data(project_id: str, data=encrypted_store) -> pd.DatataFrame:
+    return pd.DataFrame([rec for rec in data if rec["project_id"] == project_id])
